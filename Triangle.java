1457c31a33b3e64caab1d8915e2771f828cd1f26
savithripanguluri <112399080+savithripanguluri@users.noreply.github.com>
2022-11-14 14:56:40 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+public class Triangle 
+{
+    public static void main(String[] args)
+    {
+        for (int i=1; i<10; i++)
+        {
+        for (int j=i;  j<10; j++)
+        {
+            System.out.print(" * ");
+        }
+        System.out.println(" ") ;
+    }
+}
+}
